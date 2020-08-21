7d3e0dc5dc30f8e65a09b0acf93d494fc67d85d6
Atsuko Tsujioka <atsuko.tsujioka@zozo.com>
2020-08-21 17:15:04 +0900

    d

---
+++ .circleci/newline_check.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# 改行コードチェックスクリプト
+
+usage_exit() {
+    echo "Usage: $0 [-p project] [-e development|staging|production] [-r]" 1>&2
+    echo "  Options:" 1>&2
+    echo "    -e                    extention type(ex. sql, java,...)" 1>&2
+    echo "    -i                    irregular newline type(ex. LF, CRLF)" 1>&2
+    echo "    -d                    target directory(optional. default:.)" 1>&2
+    exit 1
+}
+
+EXTENTION_TYPE=""
+IRREGULAR_NEWLINE=""
+TARGET_DIRECTORY=.
+while getopts e:i:d:h OPT
+do
+    case $OPT in
+        e) EXTENTION_TYPE="${OPTARG}"
+           ;;
+        i) IRREGULAR_NEWLINE="${OPTARG}"
+           ;;
+        d) TARGET_DIRECTORY="${OPTARG}"
+           ;;
+        h) usage_exit
+           ;;
+    esac
+done
+
+if [ -z "${IRREGULAR_NEWLINE}" ]; then
+  usage_exit
+fi
+
+if [ -z "${EXTENTION_TYPE}" ]; then
+  usage_exit
+fi
+
+cnt=`find ${TARGET_DIRECTORY} -name "*.${EXTENTION_TYPE}" | xargs nkf --guess | grep \(${IRREGULAR_NEWLINE}\)  | wc -l`
+
+if [ $cnt > 0 ]; then
+    echo "${EXTENTION_TYPE}ファイルの中に改行コードが ${IRREGULAR_NEWLINE} のコードが含まれています。"
+    find ${TARGET_DIRECTORY} -name "*.${EXTENTION_TYPE}" | xargs nkf --guess | grep \(${IRREGULAR_NEWLINE}\)
+    exit 1
+fi
+
